6a64f53b4aaaaf9639ea2c3a5662fd012b33649c
Benjamin Danker <Benjamin Danker>
2024-10-21 11:06:32 -0500

    Remove unused pages and add template pages

--- es.admin/PostEdit.aspx.cs
+++ es.admin/PostEdit.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace es.admin
 {
-    public partial class About : Page
+    public partial class PostEdit : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- es.admin/PostManagement.aspx.cs
+++ es.admin/PostManagement.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace es.admin
 {
-    public partial class Contact : Page
+    public partial class PostManagement : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
